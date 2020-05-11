5d2049b124af78c697fb156d48943b2325dae2ec
Akshai M <akshai.kannan@gmail.com>
2020-05-11 20:45:57 +0530

    gnrc/pktbuf : Add tests to check Macro values

---
+++ tests/gnrc_pktbuf/main.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020 Freie Universitaet Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include <stdio.h>
+#include "net/gnrc/pktbuf.h"
+
+#define STR(x)   #x
+#define SHOW_DEFINE(x) printf("%s=%s\n", #x, STR(x))
+
+int main(void)
+{
+    SHOW_DEFINE(CONFIG_GNRC_PKTBUF_SIZE);
+}
