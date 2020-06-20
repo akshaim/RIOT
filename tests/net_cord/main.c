a40d417f0aa518476b1e567755bcaf35b24ae317
Akshai M <akshai.kannan@gmail.com>
2020-06-20 12:33:38 +0530

    sys/net/cord : Add tests

---
+++ tests/net_cord/main.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 Freie Universitaet Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include <stdio.h>
+#include "net/cord/config.h"
+
+#define STR(x)   #x
+#define SHOW_DEFINE(x) printf("%s=%s\n", #x, STR(x))
+
+int main(void)
+{
+    SHOW_DEFINE(CONFIG_CORD_LT);
+    SHOW_DEFINE(CONFIG_CORD_UPDATE_INTERVAL);
+    SHOW_DEFINE(CONFIG_CORD_EP);
+}
