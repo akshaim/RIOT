96fa2044262d5f593140e22223a44255146701c7
Akshai M <akshai.kannan@gmail.com>
2020-07-21 16:13:55 +0530

    Added tests

---
+++ tests/net_l2filter/main.c
@@ -0,0 +1,20 @@
+
+/*
+ * Copyright (c) 2020 Freie Universitaet Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include <stdio.h>
+#include "net/l2filter.h"
+
+#define STR(x)   #x
+#define SHOW_DEFINE(x) printf("%s=%s\n", #x, STR(x))
+
+int main(void)
+{
+    SHOW_DEFINE(CONFIG_L2FILTER_ADDR_MAXLEN);
+    SHOW_DEFINE(CONFIG_L2FILTER_LISTSIZE);
+}
