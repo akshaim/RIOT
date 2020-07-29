f067cb75cd6fff82dc411e4f4dea7820555fde32
Akshai M <akshai.kannan@gmail.com>
2020-07-29 16:59:27 +0530

    net/netif : Added tests

---
+++ tests/net_netif/main.c
@@ -0,0 +1,19 @@
+
+/*
+ * Copyright (c) 2020 Freie Universitaet Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include <stdio.h>
+#include "net/netif.h"
+
+#define STR(x)   #x
+#define SHOW_DEFINE(x) printf("%s=%s\n", #x, STR(x))
+
+int main(void)
+{
+    SHOW_DEFINE(CONFIG_NETIF_NAMELENMAX);
+}
