3c82cbff5b05f762ef5d08f7de039800466d2756
Akshai M <akshai.kannan@gmail.com>
2020-09-02 10:32:32 +0530

    net/sock/dtls : Update documentation

--- sys/include/net/sock/dtls.h
+++ sys/include/net/sock/dtls.h
@@ -491,14 +491,26 @@
 extern "C" {
 #endif
 
+/**
+ * @ingroup net_sock_util_conf
+ * @{
+ */
+/**
+ * @brief   Default buffer size for DTLS handshake (as exponent of 2^n)
+ *
+ * As the buffer size ALWAYS needs to be power of two, this option represents
+ * the exponent of 2^n, which will be used as the size of the buffer
+ * ( @ref DTLS_HANDSHAKE_BUFSIZE ).
+ */
 #ifndef CONFIG_DTLS_HANDSHAKE_BUFSIZE_EXP
 #define CONFIG_DTLS_HANDSHAKE_BUFSIZE_EXP  8
 #endif
+/** @} */
 
 #ifndef DTLS_HANDSHAKE_BUFSIZE
 #define DTLS_HANDSHAKE_BUFSIZE  (1 << CONFIG_DTLS_HANDSHAKE_BUFSIZE_EXP)
-                                       /**< Size buffer used in handshake to
-                                            hold credentials */
+                                        /**< Size buffer used in handshake to
+                                             hold credentials */
 #endif
 
 #define SOCK_DTLS_HANDSHAKE     (EXDEV) /**< Return value for a successful
